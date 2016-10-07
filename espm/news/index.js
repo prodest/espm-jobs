cc96ae51e99cb52892366438dbee88c6f605ebcf
alvarollmenezes <alvarollmenezes@gmail.com>
2016-10-07 19:10:49 -0300

    Atualiza para utilizar sigla pública.

--- espm/news/index.js
+++ espm/news/index.js
@@ -5,8 +5,8 @@ const orchard = require( './orchard' );
 const _parallelBlocks = process.env.PARALLEL_BLOCKS || 1;
 
 /**
- * 
- * 
+ *
+ *
  * @param {any} site
  * @param {any} lastUpdate
  * @returns
@@ -22,23 +22,40 @@ function getLastUpdate( site, lastUpdate ) {
 /**
  *
  *
+ * @param {any} sites
+ * @param {any} acronym
+ * @returns
+ */
+function getPublicAcronym( sites, acronym ) {
+    sites.filter( s => s.sigla === acronym );
+    return sites[ 0 ];
+}
+
+/**
+ *
+ *
  * @param {string} site Sigla do site
  * @returns {Promise}
  */
 function getAndIndexNews( site, lastId, lastUpdate ) {
-    return getLastUpdate( site, lastUpdate )
+    return getLastUpdate( site.sigla, lastUpdate )
         .then( lastUpdate => {
-            return orchard().getNews( site, lastUpdate );
+            return orchard().getNews( site.sigla, lastUpdate );
         } )
         .then( news => {
             if ( news.length > 0 ) {
+                news = news.map( n => {
+                    n.siglaPublica = site.siglaPublica;
+                    return n;
+                } );
+
                 return elastic().indexNews( news );
             } else {
                 return Promise.reject();
             }
         } )
         .then( lastUpdatedNews => {
-            elastic().updateLastUpdate( site, lastUpdatedNews.date );
+            elastic().updateLastUpdate( site.sigla, lastUpdatedNews.date );
 
             if ( lastId !== lastUpdatedNews.id ) {
                 return getAndIndexNews( site, lastUpdatedNews.id, lastUpdatedNews.date );
@@ -46,17 +63,10 @@ function getAndIndexNews( site, lastId, lastUpdate ) {
                 return Promise.reject();
             }
         } )
-        // .then( ( lastUpdatedNews ) => {
-        //     if ( lastId !== lastUpdatedNews.id ) {
-        //         return getAndIndexNews( site, lastUpdatedNews.id, lastUpdatedNews.date );
-        //     } else {
-        //         return Promise.reject();
-        //     }
-        // } )
         .catch( err => {
             if ( !err ) {
-                console.log( `Updated all news from ${site}.` );
-                return `Updated all news from ${site}.`;
+                console.log( `Updated all news from ${site.sigla}.` );
+                return `Updated all news from ${site.sigla}.`;
             } else {
                 throw err;
             }
@@ -71,11 +81,14 @@ elastic().createIndexesIfNotExists()
     return orchard().getSites();
 } )
 .then( sites => {
+    _highlights = _highlights.map( n => {
+        n.siglaPublica = getPublicAcronym( sites, n.siglaSite );
+        return n;
+    } );
+
     let promises = [];
     sites.forEach( site => {
-        if ( site.sigla === site.siglaPublica ) {
-            promises.push( getAndIndexNews( site.sigla ) );
-        }
+        promises.push( getAndIndexNews( site ) );
     } );
 
     const groupedPromises = [];
